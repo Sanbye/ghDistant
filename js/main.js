3fa039d15afe746aadf665fb813c7eaabd873c09
BOUSQUET Sylvain <sylvain_bousquet@connect-tech.sncf>
2023-10-25 12:50:36 +0200

    C2 extraction constantes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-const HELLO_WORLD_TITLE= "Title Hello world"
-const HELLO_WORLD_TEXT= "Hello world text"
+const HELLO_WORLD= "Hello world"
+
+const title = document.querySelector("h1");
+const bouton = document.querySelector("button");
+
+title.innerHTML = HELLO_WORLD;
+bouton.innerHTML = HELLO_WORLD;
+
+
